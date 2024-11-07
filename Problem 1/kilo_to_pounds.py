6f737c9209caff69071ae0917da57aa46e5b6677
valentina-montilla <vam6267@utulsa.edu>
2024-11-07 11:37:00 -0600

    1-3 done no errors

--- Problem 1/kilo_to_pounds.py
+++ Problem 1/kilo_to_pounds.py
@@ -1,7 +1,6 @@
 def kilo_to_pounds(kilos):
     # This statement intentionally has an error. 
-    return (kilos / 2.204)
-
+    return (kilos * 2.20462)
 
 # Main part of the program starts here. Do not remove the line below.
 if __name__ == '__main__':
--- Problem 3/stepCounter.py
+++ Problem 3/stepCounter.py
@@ -1,10 +1,11 @@
 def feet_to_steps(user_feet):
    #write your code here
-   return
+   return int(user_feet / 2.5)
 
 if __name__ == '__main__':
     #take input feet steps here
     #store it into the function
-    
+    user_feet = float(input())
     #print your steps here
     feet_to_steps(5280)
+    print(feet_to_steps(user_feet))
